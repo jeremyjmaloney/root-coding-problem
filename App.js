f168a502bce3e640410f875980ef87bc7415fb40
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-06-26 13:23:48 -0400

    Created createTrip function to add a new trip to existing driver object

--- App.js
+++ App.js
@@ -33,12 +33,33 @@ const checkData = (dataArray) => {
       createTrip(dataArray[i]);
     };
   };
+  console.log(drivers[0].trips);
 };
 
 // CREATE NEW DRIVER AND PUSH INTO DRIVERS ARRAY //
 const createDriver = (name) => {
   const driver = new Driver(name);
   drivers.push(driver);
+};
+
+const createTrip = (tripData) => {
+  let driverName = tripData[1];
+  let startTime = tripData[2];
+  let endTime = tripData[3];
+  let milesDriven = tripData[4];
+  let start = startTime.split(':');
+  let end = endTime.split(':');
+  let totalHours = (((end[0] * 60) + end[1]) - ((start[0] * 60) + start[1])) / 60;
+  let mph = milesDriven / totalHours;
+  if(mph > 5 && mph < 100) {
+    let thisDriver = drivers.find(person => person.name === driverName);
+    thisDriver.addTrip({
+      startTime: startTime,
+      endTime: endTime,
+      milesDriven: milesDriven,
+      totalHours: totalHours
+    });
+  };
 };
 
 separateData(inputData);
