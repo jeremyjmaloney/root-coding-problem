6785246827258eb8dd7793893a4e09ed4cae1e03
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-06-26 13:01:59 -0400

    Created seperateData function to separate each word in each line

--- App.js
+++ App.js
@@ -3,12 +3,25 @@ const fs = require('fs');
 let inputData = fs.readFileSync('input.txt').toString().split('\n');
 let drivers = [];
 
+// CREATE THE DRIVER CLASS WITH ATTRIBUTES FOR NAME AND ARRAY OF TRIPS //
 class Driver {
   constructor(name, trips) {
     this.name = name;
     this.trips = trips || [];
-  }
+  };
   addTrip(newTrip) {
     this.trips = [...this.trips, newTrip];
   };
 };
+
+const separateData = (data) => {
+  let dataArray = [];
+  for(let i = 0; i < data.length; i++) {
+    let line = data[i].split(' ');
+    dataArray.push(line);
+  };
+  console.log(dataArray);
+  // checkData(dataArray);
+};
+
+separateData(inputData);
