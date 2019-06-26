850efd792d478925464da42246a3dac67118a891
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-06-26 12:57:26 -0400

    Created class for a Driver that has a name and array of trips

--- App.js
+++ App.js
@@ -3,4 +3,12 @@ const fs = require('fs');
 let inputData = fs.readFileSync('input.txt').toString().split('\n');
 let drivers = [];
 
-console.log(inputData);
+class Driver {
+  constructor(name, trips) {
+    this.name = name;
+    this.trips = trips || [];
+  }
+  addTrip(newTrip) {
+    this.trips = [...this.trips, newTrip];
+  };
+};
